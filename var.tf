1e3a4afd50e17e95103cb2a238af154001a896b6
hermann <hermann042009@hotmail.com>
2022-03-07 20:38:59 -0500

    terraform script

--- var.tf
+++ var.tf
@@ -2,7 +2,7 @@ variable "userdata" {
   default = "user_data.sh"
 }
 variable "cw" {
-  default = "cw_agent_config.json"
+  default = "cw-agent.json"
 }
 variable "region" {
   default = "us-east-1"
